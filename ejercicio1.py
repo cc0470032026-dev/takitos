3090b46af5c661630bee7b6b41824142b8f048c5
Sajamary <cc04700332026@unab.edu.sv>
2026-04-09 21:31:04 -0600

    Parcial 02 Fundamentos de programación, número del ejercicio1 . Declara una variable con la cadena elefante  . 2. Utiliza el método correspondiente para eliminar los espacios en blanco a ambos extremos de la palabra.Cuenta y muestra cuántas veces se repite la letra e  en el texto ya limpio.

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+animal = "  elefante  "
+
+limpio = animal.strip()
+
+cantidad = limpio.count("e")
+
+# el resultado final inge
+print(f"Texto limpio: '{limpio}'")
+print(f"La letra 'e' se repitio {cantidad} veces, es poco xd")
