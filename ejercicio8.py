7ac0ad68f85e35ab81a4c726b33572f3bee450f3
Sajamary <cc04700332026@unab.edu.sv>
2026-04-09 22:01:58 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 8: Bloque de texto con comillas triples, conteo de letra a y division por saltos de linea con splitline

---
+++ ejercicio8.py
@@ -0,0 +1,11 @@
+poema = """Ya no la quiero, es cierto, pero tal vez la quiero.
+Es tan corto el amor, y es tan largo el olvido.
+Porque en noches como esta la tuve entre mis brazos :("""
+
+contar = poema.count("a")
+
+lista = poema.splitlines()
+
+print("Cantidad de letras 'a' encontradas:", contar)
+print("Lista de oraciones pero separadas:")
+print(lista)
