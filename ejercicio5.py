ab6432f3ff45e490ba920facf88f3d74888ca850
Sajamary <cc04700332026@unab.edu.sv>
2026-04-09 21:57:53 -0600

    Parcial 02: Ejercicios 5 y 6.

---
+++ ejercicio5.py
@@ -0,0 +1,7 @@
+palabra = "pYTHON"
+
+texto = palabra.swapcase()
+
+resultado = texto.ljust(15, "*")
+
+print("El resultado es:", resultado)
---
+++ ejercicio6.py
@@ -0,0 +1,7 @@
+texto = "william"
+
+normalizando = texto.casefold()
+
+alfabeto = normalizando.isalpha()
+
+print("¿¿¿Este texto solo son letras????:", alfabeto)
