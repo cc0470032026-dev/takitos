6d0d2886b746e6544f6076a2bdbeef2c32077ee2
Sajamary <cc04700332026@unab.edu.sv>
2026-04-09 21:40:53 -0600

    “Parcial 02 Fundamentos de programación, número del ejercicio4, . Toma la palabra CANTANDO. 2. Convierte toda la cadena a letras minusculas. 3. Elimina el sufijo ando de la palabra resultante y encuentra en que indice (posicion) quedo la letra t.

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+palabra = "CANTANDO"
+
+minusculas = palabra.lower()
+
+encontrar = minusculas.removesuffix("ando")
+
+t = encontrar.find("t")
+
+print(f"Palabra final: {encontrar}")
+print(
+    f"La letra 't' está en la posición: {t} de guapo y poderoso asombroso y muy hermoso"
+)
